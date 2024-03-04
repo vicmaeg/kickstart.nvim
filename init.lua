5978f650d791f46026c297cded7338f0c7ae48d8
Victor Martos <vmartos@outlook.com>
2024-03-05 00:14:17 +0100

    chore: map q to <Nop>

--- init.lua
+++ init.lua
@@ -164,6 +164,9 @@ vim.opt.scrolloff = 10
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Keymaps for better default experience
+vim.keymap.set('n', 'q', '<Nop>', { silent = true })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
