0598988eadc8f57c93f51e3ebb7d05a070f194ed
Victor Martos <v.martos@fourthline.com>
2024-08-09 16:48:31 +0200

    feat: configure telescope mappings and start without preview

--- init.lua
+++ init.lua
@@ -385,11 +385,19 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<c-t>'] = require('telescope.actions.layout').toggle_preview,
+            },
+            n = {
+              ['t'] = require('telescope.actions.layout').toggle_preview,
+            },
+          },
+          preview = {
+            hide_on_startup = true,
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
