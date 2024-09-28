cc6a8613aacee72a033feaaee90c352709533fc6
vicmaeg <vmartos@outlook.com>
2024-09-28 15:15:03 +0200

    feat: add roslyn plugin for .NET development

---
+++ lua/custom/plugins/roslyn.lua
@@ -0,0 +1,8 @@
+return {
+  -- use roslyn custom plugin for .NET LSP
+  'seblj/roslyn.nvim',
+  ft = 'cs',
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
