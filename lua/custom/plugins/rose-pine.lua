9ed841c6073639e43a5fc5c096b75de400f1a515
vicmaeg <vmartos@outlook.com>
2024-10-02 20:24:49 +0200

    feat: use rose-pine colorscheme

---
+++ lua/custom/plugins/rose-pine.lua
@@ -0,0 +1,13 @@
+return {
+  'rose-pine/neovim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'rose-pine'
+
+    -- You can configure highlights by doing something like:
+    -- vim.cmd.hi 'Comment gui=none'
+  end,
+}
